8df0dfcb2a462d6085e9961351e5c860e4d96e64
Aleksey <sphota9@gmail.com>
2017-05-11 00:14:55 -0400

    Initial Commit

---
+++ intellex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  intellex
+//
+//  Created by sphota on 5/11/17.
+//  Copyright © 2017 sphota. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
